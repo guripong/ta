d25f6c959a6668233726da390badf2935504e253
john.so <john.so@koreapolyschool.com>
2019-01-18 15:54:53 +0900

    124

--- ta.js
+++ ta.js
@@ -36,7 +36,7 @@ app.post("/echo", function(req, res) {
 //<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>
   var audio1 = `<audio src="https://s3.amazonaws.com/eduai/common/common-003-correct-good.mp3"/>`;
   if (intent == 'Intent_Quiz') {
-    if(speech)response = `<speak>hi audio test! number 1. ${audio1} ${audio1} ${audio1} ${audio1} ${audio1}</speak> `;
+    if(speech)response = `<speak>hi audio test! number 1. ${audio1} ${audio1} ${audio1} ${audio1} ${audio1} ${audio1} ${audio1}</speak> `;
     
    // "<speak> <audio src='https://s3.amazonaws.com/eduai/sw01/u01/p01/sw01-u01-p01-001-wp-title.mp3'>did not get your audio file</audio> you said that "+speech+". say anything!<speak>";
     else response ='say anything!';
