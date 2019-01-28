3510235a0623d8ad8e942387793746753ba36a98
john.so <john.so@koreapolyschool.com>
2019-01-28 15:39:58 +0900

    1234

--- ta.js
+++ ta.js
@@ -75,12 +75,6 @@ app.post("/allintent", function(req, res) {
           title: "card title",
           subtitle: "card text",
           imageUri: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-          buttons: [
-            {
-              text: "button text",
-              postback: "https://assistant.google.com/"
-            }
-          ]
         }
       }
     ],
