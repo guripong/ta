b86c3173c1a62c2aaa009f1ff12bc8c2faa51898
john.so <john.so@koreapolyschool.com>
2019-01-28 16:20:55 +0900

    1234

--- ta.js
+++ ta.js
@@ -68,59 +68,18 @@ app.post("/allintent", function(req, res) {
     
   return res.json({
     fulfillmentText: "This is a text response",
-    fulfillmentMessages: [
-      {
-        card: {
-          title: "card title",
-          subtitle: "card text",
-          imageUri: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-          buttons: [
-            {
-              text: "button text",
-              postback: "https://assistant.google.com/"
-            }
-          ]
-        }
+    fulfillmentMessages: [{
+      platform: "ACTIONS_ON_GOOGLE",
+      text: {
+        "text": [
+          "Text response"
+        ]
       }
-    ],
+    }],
     source: "example.com",
-    payload: {
-      google: {
-        expectUserResponse: true,
-        richResponse: {
-          items: [
-            {
-              simpleResponse: {
-                textToSpeech: "this is a simple response"
-              }
-            }
-          ]
-        }
-      },
-      facebook: {
-        text: "Hello, Facebook!"
-      },
-      slack: {
-        text: "This is a text response for Slack."
-      }
-    },
-    outputContexts: [
-      {
-        name: "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-        lifespanCount: 5,
-        parameters: {
-          param: "param value"
-        }
-      }
-    ],
-    followupEventInput: {
-      name: "event name",
-      languageCode: "en-US",
-      parameters: {
-        param: "param value"
-      }
-    }
-  });
+    
+  }
+  );
   /*
    return res.json({
     fulfillmentText: response,
