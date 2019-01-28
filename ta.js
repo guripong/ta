9b17e1eb56aea647e98acb42b168746edb2b636c
john.so <john.so@koreapolyschool.com>
2019-01-28 16:16:29 +0900

    !234

--- ta.js
+++ ta.js
@@ -67,57 +67,57 @@ app.post("/allintent", function(req, res) {
   
     
   return res.json({
-    "fulfillmentText": "This is a text response",
-    "fulfillmentMessages": [
+    fulfillmentText: "This is a text response",
+    fulfillmentMessages: [
       {
-        "card": {
-          "title": "card title",
-          "subtitle": "card text",
-          "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-          "buttons": [
+        card: {
+          title: "card title",
+          subtitle: "card text",
+          imageUri: "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+          buttons: [
             {
-              "text": "button text",
-              "postback": "https://assistant.google.com/"
+              text: "button text",
+              postback: "https://assistant.google.com/"
             }
           ]
         }
       }
     ],
-    "source": "example.com",
-    "payload": {
-      "google": {
-        "expectUserResponse": true,
-        "richResponse": {
-          "items": [
+    source: "example.com",
+    payload: {
+      google: {
+        expectUserResponse: true,
+        richResponse: {
+          items: [
             {
-              "simpleResponse": {
-                "textToSpeech": "this is a simple response"
+              simpleResponse: {
+                textToSpeech: "this is a simple response"
               }
             }
           ]
         }
       },
-      "facebook": {
-        "text": "Hello, Facebook!"
+      facebook: {
+        text: "Hello, Facebook!"
       },
-      "slack": {
-        "text": "This is a text response for Slack."
+      slack: {
+        text: "This is a text response for Slack."
       }
     },
-    "outputContexts": [
+    outputContexts: [
       {
-        "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-        "lifespanCount": 5,
-        "parameters": {
-          "param": "param value"
+        name: "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+        lifespanCount: 5,
+        parameters: {
+          param: "param value"
         }
       }
     ],
-    "followupEventInput": {
-      "name": "event name",
-      "languageCode": "en-US",
-      "parameters": {
-        "param": "param value"
+    followupEventInput: {
+      name: "event name",
+      languageCode: "en-US",
+      parameters: {
+        param: "param value"
       }
     }
   });
