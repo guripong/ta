8b32f32a2354822b8ac4ddd718fd9613c221cc0b
john.so <john.so@koreapolyschool.com>
2019-04-24 10:38:22 +0900

    1234

--- route/power_reading.js
+++ route/power_reading.js
@@ -240,43 +240,52 @@ ap.intent('Answer', (conv, input,option) => {
 
     }
     else if(speak.indexOf('type 6')!==-1 || speak.indexOf('type six')!==-1){ //터치가능
-        conv.ask(new SimpleResponse({
-          speech:`you said that ${speak}. here is new example`,
-          text:`haha`,
-        }));
-      //  conv.ask(new Suggestions(kind_of_suggestions));
-        conv.ask(new BrowseCarousel({
-          items: [
-            new BrowseCarouselItem({
-              title: 'Title of item 1',
-              url: 'google.com',
-              description: 'Description of item 1',
-              image: new Image({
-                url: 'https://s3.amazonaws.com/eduai/test_image/cat3.PNG',
-                alt: 'Image alternate text',
-              }),
-              footer: 'Item 1 footer',
-            }),
-            new BrowseCarouselItem({
-              title: 'Google Assistant',
-              url: 'google.com',
-              description: 'Google Assistant on Android and iOS',
-              image: new Image({
-                url: 'https://s3.amazonaws.com/eduai/test_image/cat3.PNG',
-                alt: 'Image alternate text',
-              }),
-              footer: 'More information about the Google Assistant',
-            }),
-          ],
-        }));
-        /*
-        conv.ask(new SimpleResponse({
-          speech: 'This is the second simple response.',
-          text: 'This is the 2nd simple response.',
-        }));
-        */
-        console.log('############################');
-        console.log(conv);  
+       return res.json({
+        "payload": {
+          "google": {
+            "expectUserResponse": true,
+            "richResponse": {
+              "items": [
+                {
+                  "simpleResponse": {
+                    "textToSpeech": "This is a browse carousel example."
+                  }
+                },
+                {
+                  "carouselBrowse": {
+                    "items": [
+                      {
+                        "title": "Title of item 1",
+                        "openUrlAction": {
+                          "url": "google.com"
+                        },
+                        "description": "Description of item 1",
+                        "footer": "Item 1 footer",
+                        "image": {
+                          "url": "IMG_URL.com",
+                          "accessibilityText": "Image alternate text"
+                        }
+                      },
+                      {
+                        "title": "Google Assistant",
+                        "openUrlAction": {
+                          "url": "google.com"
+                        },
+                        "description": "Google Assistant on Android and iOS",
+                        "footer": "More information about the Google Assistant",
+                        "image": {
+                          "url": "IMG_URL_Assistant.com",
+                          "accessibilityText": "Image alternate text"
+                        }
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        }
+      });
     }
     /*
     else if(speak.indexOf('type 6')!==-1 || speak.indexOf('type six')!==-1){
